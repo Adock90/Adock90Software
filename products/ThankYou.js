e59f1a633b93fbb036200ed6ab5a3c2ac9a1a175
Adam Croft <169613052+Adock90@users.noreply.github.com>
2026-05-28 11:40:14 +0100

    Update ThankYou.js

--- products/ThankYou.js
+++ products/ThankYou.js
@@ -1,4 +1,18 @@
 
+
+
+function MatchTheme(){
+    let textcolourfirsttitle = document.getElementById("firsttitle");
+    let textcoloursecondtitle = document.getElementById("secondtitle");
+    let textcolourcountdown = document.getElementById("countdown");
+    if (getComputedStyle(document.body).backgroundColor === "rgb(41, 41, 41)"){
+        textcolourcountdown.style.color = 'white';
+        textcolourfirsttitle.style.color = 'white';
+        textcoloursecondtitle.style.color = 'white';
+    }
+
+}
+
 function LocateBack() {
     let TimerObj = document.getElementById("countdown");
     let TimeLeft = parseInt(TimerObj.innerHTML);
@@ -11,6 +25,6 @@ function LocateBack() {
     
 }
 
-
+window.addEventListener("load", MatchTheme());
 
 setInterval(LocateBack, 1000);
