690f8b94cd3e2e0cf5fe0a341338d8d53bfff6cc
Adam Croft <169613052+Adock90@users.noreply.github.com>
2026-05-28 01:25:56 +0100

    Add files via upload

---
+++ ThankYou.js
@@ -0,0 +1,27 @@
+function MatchTheme(){
+    let textcolourfirsttitle = document.getElementById("firsttitle");
+    let textcoloursecondtitle = document.getElementById("secondtitle");
+    let textcolourcountdown = document.getElementById("countdown");
+    if (getComputedStyle(document.body).backgroundColor === "rgb(41, 41, 41)"){
+        textcolourcountdown.style.color = 'white';
+        textcolourfirsttitle.style.color = 'white';
+        textcoloursecondtitle.style.color = 'white';
+    }
+
+}
+
+function LocateBack() {
+    let TimerObj = document.getElementById("countdown");
+    let TimeLeft = parseInt(TimerObj.innerHTML);
+    if (1 === TimeLeft){
+        window.location.href = "../index.html";
+    } else {
+        TimeLeft--;
+        TimerObj.innerHTML = TimeLeft.toString();
+    }
+    
+}
+
+MatchTheme();
+
+setInterval(LocateBack, 1000);
